8805b369b0ac50904bf11ccdb4b295885eb20510
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 01:53:06 -0300

    feat: create service email

---
+++ api-email/src/main/java/apimensageria/apiemail/services/EmailService.java
@@ -0,0 +1,50 @@
+package apimensageria.apiemail.services;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import apimensageria.apiemail.entities.EmailEntity;
+import apimensageria.apiemail.entities.enums.StatusSend;
+import apimensageria.apiemail.repositories.EmailRepository;
+
+@Service
+public class EmailService {
+
+  @Value("${spring.mail.username}")
+  private String emailEnterprise;
+
+  private EmailRepository emailRepository;
+
+  private JavaMailSender emailSender;
+
+  public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
+    this.emailRepository = emailRepository;
+    this.emailSender = emailSender;
+  }
+
+  public EmailEntity sendEmail(EmailEntity emailEntity) {
+    emailEntity.setSendDateEmail(LocalDateTime.now());
+    try {
+      SimpleMailMessage message = new SimpleMailMessage();
+      message.setFrom(emailEnterprise);
+      message.setTo(emailEntity.getEmailPessoa(), emailEntity.getEmailEmpresa());
+      message.setSubject(emailEntity.getAssunto());
+      message.setText(emailEntity.getMensagem());
+
+      emailSender.send(message);
+
+      emailEntity.setStatusSend(StatusSend.SENT);
+    } catch (MailException e) {
+      emailEntity.setStatusSend(StatusSend.ERROR);
+    } finally {
+      this.emailRepository.save(emailEntity);
+    }
+
+    return emailEntity;
+  }
+}
