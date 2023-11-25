37d515a4da1f721e1479c0c9c2ed77e37ad5a2f0
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 20:25:45 -0300

    feat: create component formContato

---
+++ interface-emerson/interface-empresa/src/app/modules/components/form-contato/form-contato.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IEmpresa } from 'src/app/interfaces/IEmpresa';
+import { IFaleConosco } from 'src/app/interfaces/IFaleConosco';
+import { EmpresaService } from 'src/app/services/empresa.service';
+
+@Component({
+  selector: 'app-form-contato',
+  templateUrl: './form-contato.component.html',
+  styleUrls: ['./form-contato.component.scss'],
+})
+export class FormContatoComponent implements OnInit {
+  public listEmpresa: IEmpresa[] = [];
+
+  form: FormGroup = this.formBuilder.group({
+    nomePessoa: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    assunto: ['', Validators.required],
+    mensagem: ['', Validators.required],
+    IdEmpresa: ['', Validators.required],
+  });
+
+  constructor(
+    private empresaService: EmpresaService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.empresaService.empresaList().subscribe({
+      next: (res) => {
+        this.listEmpresa = res;
+        this.form.addControl('IdEmpresa', this.formBuilder.control(''));
+      },
+      error: (err) => console.log(err),
+    });
+  }
+
+  public enviarPergunta(empresa: IFaleConosco, id: number) {
+    this.empresaService.enviarPergunta(empresa, id).subscribe({
+      next: (res) => res,
+      error: (err) => console.log(err),
+    });
+  }
+
+  public submitForm() {
+    const { IdEmpresa, ...pergunta } = this.form.value;
+
+    if (this.form.valid) {
+      this.enviarPergunta(pergunta, IdEmpresa);
+    }
+  }
+}
