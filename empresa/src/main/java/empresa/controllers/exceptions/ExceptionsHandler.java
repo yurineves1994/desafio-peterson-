bc69e8f51647829c60c7c54dac30ca394ab2d43c
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 01:58:06 -0300

    feat: error handling

---
+++ empresa/src/main/java/empresa/controllers/exceptions/ExceptionsHandler.java
@@ -0,0 +1,33 @@
+package empresa.controllers.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import empresa.services.exceptions.DataIntegratyViolationException;
+import empresa.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ExceptionsHandler {
+
+  @ExceptionHandler(ObjectNotFoundException.class)
+  public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+    StandardError err = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), e.getMessage(),
+        request.getRequestURI());
+
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+  }
+
+  @ExceptionHandler(DataIntegratyViolationException.class)
+  public ResponseEntity<StandardError> DataIntegratyViolationException(DataIntegratyViolationException e, HttpServletRequest request) {
+
+    StandardError err = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+        request.getRequestURI());
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+  }
+}
---
+++ empresa/src/main/java/empresa/services/exceptions/DataIntegratyViolationException.java
@@ -0,0 +1,9 @@
+package empresa.services.exceptions;
+
+public class DataIntegratyViolationException extends RuntimeException {
+
+  public DataIntegratyViolationException(String message) {
+    super(message);
+  }
+
+}
