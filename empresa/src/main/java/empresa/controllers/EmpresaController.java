b34faa07dafe443b92ac60dcea6f6fa159bfa861
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 20:29:38 -0300

    fix: adjusted backend

--- empresa/src/main/java/empresa/controllers/EmpresaController.java
+++ empresa/src/main/java/empresa/controllers/EmpresaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import empresa.services.EmpresaService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/empresas")
 public class EmpresaController {
 
@@ -28,6 +30,7 @@ public class EmpresaController {
 
     @PostMapping
     public ResponseEntity<?> cadastrarEmpresa(@RequestBody @Valid Empresa empresa) {
+        System.out.println(empresa);
 
         List<Empresa> empresas = empresaService.cadastrarEmpresa(empresa);
 
--- empresa/src/main/java/empresa/controllers/FaleConoscoController.java
+++ empresa/src/main/java/empresa/controllers/FaleConoscoController.java
@@ -2,6 +2,7 @@ package empresa.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import empresa.entities.FaleConosco;
 import empresa.services.FaleConoscoService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/faleconosco")
 public class FaleConoscoController {
 
