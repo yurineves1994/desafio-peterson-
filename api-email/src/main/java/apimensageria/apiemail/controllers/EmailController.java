e5f9119be0747f2b0cf6118f3d8f7f23cf159795
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 01:53:24 -0300

    feat: create controller email

---
+++ api-email/src/main/java/apimensageria/apiemail/controllers/EmailController.java
@@ -0,0 +1,34 @@
+package apimensageria.apiemail.controllers;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import apimensageria.apiemail.dtos.EmailDTO;
+import apimensageria.apiemail.entities.EmailEntity;
+import apimensageria.apiemail.services.EmailService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/ms")
+public class EmailController {
+
+  private EmailService emailService;
+
+  @PostMapping
+  public ResponseEntity<EmailEntity> sendEmail(@RequestBody @Valid EmailDTO emailDTO) {
+    System.out.println(emailDTO);
+    EmailEntity emailEntity = new EmailEntity();
+    BeanUtils.copyProperties(emailDTO, emailEntity);
+    emailService.sendEmail(emailEntity);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(emailEntity);
+  }
+
+}
