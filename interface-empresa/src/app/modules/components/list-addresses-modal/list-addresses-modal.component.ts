d1bba30bd08774b1724fc639af8acaff364ad378
Yuri Neves <yurineves1934@gmail.com>
2023-11-25 21:52:16 -0300

    feat: implementation of excel download service with list of companies and addresses

--- interface-empresa/src/app/modules/components/list-addresses-modal/list-addresses-modal.component.ts
+++ interface-empresa/src/app/modules/components/list-addresses-modal/list-addresses-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IEmpresa, IEndereco } from 'src/app/interfaces/IEmpresa';
+import { IEndereco } from 'src/app/interfaces/IEmpresa';
 import { EmpresaService } from 'src/app/services/empresa.service';
+
+import { saveAs } from 'file-saver';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-list-addresses-modal',
@@ -16,5 +19,44 @@ export class ListAddressesModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.enderecos = this.empresas[this.empresaId - 1].enderecos;
+  }
+
+  exportEmpresasToExcel() {
+    let listEnderecos = this.enderecos;
+
+    let wb = XLSX.utils.book_new();
+
+    let ws_data = [
+      ['Id', 'Logadouro', 'Bairro', 'Cidade', 'Estado', 'Latitude', 'Logitude', 'CEP' ],
+      ...listEnderecos.map((empresa:IEndereco) => [
+        empresa.id,
+        empresa.logradouro,
+        empresa.bairro,
+        empresa.cidade,
+        empresa.estado,
+        empresa.latitude,
+        empresa.longitude,
+        empresa.cep,
+      ]),
+    ];
+
+    let ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Empresas');
+
+    let wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    let blob = new Blob([this.s2ab(wbout)], {
+      type: 'application/octet-stream',
+    });
+
+    saveAs(blob, `enderecos-clienteId-${this.empresaId - 1}.xlsx`);
+  }
+
+  s2ab(s: any) {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+    return buf;
   }
 }
--- interface-empresa/src/app/modules/components/list-empresas/list-empresas.component.ts
+++ interface-empresa/src/app/modules/components/list-empresas/list-empresas.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IEmpresa } from 'src/app/interfaces/IEmpresa';
 import { ModalService } from 'src/app/services/modal.service';
 
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+
 @Component({
   selector: 'app-list-empresas',
   templateUrl: './list-empresas.component.html',
@@ -50,5 +53,41 @@ export class ListEmpresasComponent implements OnInit {
   closeListAddressesModal() {
     this.isListAddressesModalOpen = false;
     this.modalService.close();
+  }
+
+  exportEmpresasToExcel() {
+    let empresas = this.listEmpresa;
+
+    let wb = XLSX.utils.book_new();
+
+    let ws_data = [
+      ['Id', 'Razão Social', 'Nome Fantasia', 'E-mail', 'CNPJ'],
+      ...empresas.map((empresa) => [
+        empresa.id,
+        empresa.razaoSocial,
+        empresa.nomeFantasia,
+        empresa.email,
+        empresa.cnpj,
+      ]),
+    ];
+
+    let ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Empresas');
+
+    let wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    let blob = new Blob([this.s2ab(wbout)], {
+      type: 'application/octet-stream',
+    });
+
+    saveAs(blob, 'empresas.xlsx');
+  }
+
+  s2ab(s: any) {
+    let buf = new ArrayBuffer(s.length);
+    let view = new Uint8Array(buf);
+    for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+    return buf;
   }
 }
