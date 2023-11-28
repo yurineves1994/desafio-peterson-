f067a23f1cb548ece74e8d19aae45a897731ea36
Yuri Neves <yurineves1934@gmail.com>
2023-11-27 21:33:25 -0300

    feat: feat: add log system to the email service application

--- api-email/src/main/java/apimensageria/apiemail/controllers/EmailController.java
+++ api-email/src/main/java/apimensageria/apiemail/controllers/EmailController.java
@@ -1,5 +1,7 @@
 package apimensageria.apiemail.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,10 +21,14 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/ms")
 public class EmailController {
 
+  private static final Logger logger = LogManager.getLogger(EmailController.class);
+
   private EmailService emailService;
 
   @PostMapping
   public ResponseEntity<EmailEntity> sendEmail(@RequestBody @Valid EmailDTO emailDTO) {
+    logger.info("INFORMAÇÕES RECEBIDAS");
+    
     EmailEntity emailEntity = new EmailEntity();
     BeanUtils.copyProperties(emailDTO, emailEntity);
     emailService.sendEmail(emailEntity);
--- api-email/src/main/java/apimensageria/apiemail/services/EmailService.java
+++ api-email/src/main/java/apimensageria/apiemail/services/EmailService.java
@@ -4,18 +4,22 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import apimensageria.apiemail.controllers.EmailController;
 import apimensageria.apiemail.entities.EmailEntity;
 import apimensageria.apiemail.entities.enums.StatusSend;
 import apimensageria.apiemail.repositories.EmailRepository;
 
 @Service
 public class EmailService {
+  private static final Logger logger = LogManager.getLogger(EmailService.class);
 
   @Value("${spring.mail.username}")
   private String emailEnterprise;
@@ -46,11 +50,14 @@ public class EmailService {
         emailSender.send(message);
       }
 
+      logger.info("EMAIL ENVIADO COM SUCESSO!");
       emailEntity.setStatusSend(StatusSend.SENT);
     } catch (MailException e) {
+      logger.error("ERRO NO ENVIO DO EMAIL: " + e);
       emailEntity.setStatusSend(StatusSend.ERROR);
     } finally {
       this.emailRepository.save(emailEntity);
+      logger.info("EMAIL SALVO NO BANCO DE DADOS!");
     }
 
     return emailEntity;
