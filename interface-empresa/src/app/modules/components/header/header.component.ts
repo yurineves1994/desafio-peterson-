285cbfa19db98310e931f1f65eca685607791d66
Yuri Neves <yurineves1934@gmail.com>
2023-11-27 22:11:27 -0300

    fix: logout service creation

--- interface-empresa/src/app/modules/components/header/header.component.ts
+++ interface-empresa/src/app/modules/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) {}
 
+  logout() {
+    this.authService.logout();
+  }
 }
